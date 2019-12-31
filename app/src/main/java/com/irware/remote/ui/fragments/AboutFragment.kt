dbbb7c6d35dc5bf3466efc02480fae744c348d1c
Uday Hegde <udayhegde.mca18@bmsce.ac.in>
2019-12-31 08:48:12 +0530

    update about details

--- app/src/main/java/com/irware/remote/ui/fragments/AboutFragment.kt
+++ app/src/main/java/com/irware/remote/ui/fragments/AboutFragment.kt
@@ -1,21 +1,44 @@
 package com.irware.remote.ui.fragments
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import com.irware.remote.R
 
 class AboutFragment : androidx.fragment.app.Fragment() {
     private var listener: OnFragmentInteractionListener? = null
+    private var rootView: ScrollView? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if(rootView == null ){
+            rootView = inflater.inflate(R.layout.fragment_about, container, false) as ScrollView
+            val about = rootView!!.findViewById<TextView>(R.id.about_irware)
+            about.text = TextUtils.join(" ",context?.resources?.getStringArray(R.array.about)?: arrayOf(" "))
+
+            val onClickListener = View.OnClickListener {
+                val browserIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://www.apache.org/licenses/LICENSE-2.0")
+                )
+                startActivity(browserIntent)
+            }
+            val fabLicense = rootView!!.findViewById(R.id.fab_license) as LinearLayout
+            fabLicense.setOnClickListener(onClickListener)
+            val pikoloLicense = rootView!!.findViewById(R.id.pikolo_license) as LinearLayout
+            pikoloLicense.setOnClickListener(onClickListener)
+            val materialLicense = rootView!!.findViewById(R.id.material_license) as LinearLayout
+            materialLicense.setOnClickListener(onClickListener)
+        }
+
+        return rootView
     }
 
     override fun onAttach(context: Context) {
